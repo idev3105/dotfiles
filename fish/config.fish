c2aede6695642d5ca6a14a7676b941112ca4d108
idev <khanhcongpruj123@gmail.com>
2023-12-25 14:46:00 +0700

    update variable enviroment

--- fish/config.fish
+++ fish/config.fish
@@ -8,4 +8,56 @@ if status is-interactive
     # You must install StartShip
     # https://starship.rs/
     starship init fish | source
+
+    # enviroment variable
+    # go
+    set -U GOPATH $HOME/Documents/mybin/go/v1.20.5
+    set -x PATH $PATH $GOPATH/bin
+    set -x PATH $PATH $HOME/go/bin
+
+    # node
+    set -x PATH $PATH $HOME/Documents/mybin/node/node-v18.16.0-darwin-arm64/bin
+    set -U NPM_TOKEN XXXXX-XXXXX-XXXXX-XXXXX
+
+    # protoc
+    set -x PATH $PATH $HOME/Documents/mybin/protoc/v23.2/bin
+
+    # psql
+    set -x PATH $PATH $HOME/Documents/mybin/postgresql/v15.3.2/bin
+
+    # yarn
+    set -x PATH $PATH $(yarn global bin)
+
+    # gradle
+    set -x PATH $PATH $HOME/Documents/mybin/gradle-8.3/bin
+
+    # editor
+    set -U EDITOR nvim
+    set -U VISUAL nvim
+
+    # jabba
+    if test -s "/Users/idev/.jabba/jabba.fish"
+        source "/Users/idev/.jabba/jabba.fish"
+    end
+
+    # bun
+    set -U BUN_INSTALL $HOME/.bun
+    set -x PATH $PATH $BUN_INSTALL/bin
+
+    # zig
+    set -x PATH $PATH $HOME/Documents/mybin/zig-macos-aarch64-0.12.0-dev.627+de4d1ea25
+
+    # android
+    set -U ANDROID_HOME $HOME/Library/Android/sdk
+    set -U PATH $PATH $ANDROID_HOME/emulator
+    set -U PATH $PATH $ANDROID_HOME/platform-tools
+
+    set -U PATH /opt/homebrew/opt/ruby/bin:$PATH
+    set -U LDFLAGS -L/opt/homebrew/opt/ruby/lib
+    set -U CPPFLAGS -I/opt/homebrew/opt/ruby/include
+
+    # neovim
+    set -x PATH $PATH $HOME/.local/share/nvim/mason/bin
+
+    set -U XDG_CONFIG_HOME $HOME/.config
 end
