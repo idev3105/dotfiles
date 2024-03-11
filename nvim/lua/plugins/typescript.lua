a7d8535b8f2f57192ccbfe33561fc6f61cd66377
idev <khanhcongpruj123@gmail.com>
2024-03-11 21:52:17 +0700

    update

--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -42,12 +42,6 @@ return {
           },
         }
       end
-
-      -- Load debug config from .vscode/launch.json
-      require("dap.ext.vscode").load_launchjs(nil, {
-        node = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
-        ["pwa-node"] = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
-      })
     end,
   },
 }
